49cec2c602683e7d7cb8ab3afb251b24dcf6614a
Benson <bensonkimkam@gmail.com>
2022-06-14 12:53:35 +0300

    added rating component

--- Javascript/React/react-redux-themoviedb/src/Components/Home.jsx
+++ Javascript/React/react-redux-themoviedb/src/Components/Home.jsx
@@ -1,32 +1,49 @@
 import React, { useEffect, useState } from 'react'
+// import { IoStar, IoStarHalf } from 'react-icons/io'
+import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
 import axios from 'axios'
 import Main from './Main'
 import Navbar from './Navbar'
 
-const api_key = 'd6ca859e565544668ce9853ec7cf1104'
+// const api_key = 'd6ca859e565544668ce9853ec7cf1104'
 
 const Home = () => {
 
-    const [movies, setMovies] = useState('')
-    const url = 'https://api.themoviedb.org/3/movie/550/lists?api_key=d6ca859e565544668ce9853ec7cf1104&language=en-US&page=1'
-    
-    useEffect(() => {
-        setMovies()
-    }, [])
-    
-    const getMovies = () => {
-        axios.get(url).then((res) => {
-            const allMovies = res.data
-            setMovies(allMovies)
-            // console.log(allMovies)
-        }).catch((error)=> console.log(`Error: ${error}`))
-    }
-  return (
-      <div>
-          {/* {getMovies()} */}
-          <Navbar />
+  const [movies, setMovies] = useState('')
+  const url = 'https://api.themoviedb.org/3/trending/movie/week?api_key=d6ca859e565544668ce9853ec7cf1104'
 
-          <Main movies={movies} />
+  useEffect(() => {
+    axios.get(url)
+      .then(res => setMovies(res.data.results))
+  }, [])
+
+  const getStars = (rating) => {
+
+    // Round to nearest half
+    rating = Math.round(rating / 2);
+    let output = [];
+    // Append all the filled whole stars
+    for (var i = rating; i >= 1; i--)
+      output.push(<BsStarFill className='text-blue-700 ' /> );
+
+    // If there is a half a star, append it
+    if (i == .5) output.push(<BsStarHalf className='text-blue-700' />);
+
+    // Fill the empty stars
+    for (let i = (5 - rating); i >= 1; i--)
+      output.push(<BsStar className='text-blue-700' />);
+
+    return output
+
+  }
+
+
+
+  return (
+    <div>
+      <Navbar />
+
+      <Main movies={movies} getStars={getStars} />
     </div>
   )
 }
--- Javascript/React/react-redux-themoviedb/src/Components/Movies.jsx
+++ Javascript/React/react-redux-themoviedb/src/Components/Movies.jsx
@@ -2,122 +2,29 @@ import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { BsFillStarFill } from 'react-icons/bs'
 
-const Movies = ({ movies }) => {
+const Movies = ({ movies, getStars }) => {
     // console.log(movies);
+
     return (
         <div className='bg-gray-900 grow'>
-            <h2 className='capitalize text-slate-100 text-lg m-3'>Discover</h2>
+            <h2 className='capitalize text-slate-100 text-lg m-3'>Trending</h2>
             <div className='my-4 mx-8 grid grid-cols-3 gap-x-5 gap-y-8 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6'>
-                <Link to='' className='rounded bg-inherit'>
-                    <img src="https://images.pexels.com/photos/7976054/pexels-photo-7976054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""
+            {movies?movies.map((movie)=>(
+                <Link to='' key={movie.id} className='rounded bg-inherit flex flex-wrap flex-col'>
+                   {console.log(movie)}
+                    <img src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path} alt=""
                         className='h-48 w-full rounded' />
                     <div className='w-full text-sm py-2'>
-                        <p className='text-slate-200'>Wonder woman</p>
+                        <p className='text-slate-200'>{movie.title}</p>
                         <span className='flex justify-between items-center'>
-                            <p className='flex items-center gap-1 text-blue-700'><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /></p>
+                            <p className='flex items-center gap-1 text-blue-700'>{getStars(movie.vote_average)}</p>
+                            {/* <p className='flex items-center gap-1 text-blue-700'><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /></p> */}
                             <h2 className='text-slate-200'>90 min</h2>
                         </span>
                     </div>
                 </Link>
-                <Link to='' className='rounded bg-inherit'>
-                    <img src="https://images.pexels.com/photos/7976054/pexels-photo-7976054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""
-                        className='h-48 w-full rounded' />
-                    <div className='w-full text-sm py-2'>
-                        <p className='text-slate-200'>Wonder woman</p>
-                        <span className='flex justify-between items-center'>
-                            <p className='flex items-center gap-1 text-blue-700'><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /></p>
-                            <h2 className='text-slate-200'>90 min</h2>
-                        </span>
-                    </div>
-                </Link>
-                <Link to='' className='rounded bg-inherit'>
-                    <img src="https://images.pexels.com/photos/7976054/pexels-photo-7976054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""
-                        className='h-48 w-full rounded' />
-                    <div className='w-full text-sm py-2'>
-                        <p className='text-slate-200'>Wonder woman</p>
-                        <span className='flex justify-between items-center'>
-                            <p className='flex items-center gap-1 text-blue-700'><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /></p>
-                            <h2 className='text-slate-200'>90 min</h2>
-                        </span>
-                    </div>
-                </Link>
-                <Link to='' className='rounded bg-inherit'>
-                    <img src="https://images.pexels.com/photos/7976054/pexels-photo-7976054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""
-                        className='h-48 w-full rounded' />
-                    <div className='w-full text-sm py-2'>
-                        <p className='text-slate-200'>Wonder woman</p>
-                        <span className='flex justify-between items-center'>
-                            <p className='flex items-center gap-1 text-blue-700'><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /></p>
-                            <h2 className='text-slate-200'>90 min</h2>
-                        </span>
-                    </div>
-                </Link>
-                <Link to='' className='rounded bg-inherit'>
-                    <img src="https://images.pexels.com/photos/7976054/pexels-photo-7976054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""
-                        className='h-48 w-full rounded' />
-                    <div className='w-full text-sm py-2'>
-                        <p className='text-slate-200'>Wonder woman</p>
-                        <span className='flex justify-between items-center'>
-                            <p className='flex items-center gap-1 text-blue-700'><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /></p>
-                            <h2 className='text-slate-200'>90 min</h2>
-                        </span>
-                    </div>
-                </Link>
-                <Link to='' className='rounded bg-inherit'>
-                    <img src="https://images.pexels.com/photos/7976054/pexels-photo-7976054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""
-                        className='h-48 w-full rounded' />
-                    <div className='w-full text-sm py-2'>
-                        <p className='text-slate-200'>Wonder woman</p>
-                        <span className='flex justify-between items-center'>
-                            <p className='flex items-center gap-1 text-blue-700'><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /></p>
-                            <h2 className='text-slate-200'>90 min</h2>
-                        </span>
-                    </div>
-                </Link>
-                <Link to='' className='rounded bg-inherit'>
-                    <img src="https://images.pexels.com/photos/7976054/pexels-photo-7976054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""
-                        className='h-48 w-full rounded' />
-                    <div className='w-full text-sm py-2'>
-                        <p className='text-slate-200'>Wonder woman</p>
-                        <span className='flex justify-between items-center'>
-                            <p className='flex items-center gap-1 text-blue-700'><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /></p>
-                            <h2 className='text-slate-200'>90 min</h2>
-                        </span>
-                    </div>
-                </Link>
-                <Link to='' className='rounded bg-inherit'>
-                    <img src="https://images.pexels.com/photos/7976054/pexels-photo-7976054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""
-                        className='h-48 w-full rounded' />
-                    <div className='w-full text-sm py-2'>
-                        <p className='text-slate-200'>Wonder woman</p>
-                        <span className='flex justify-between items-center'>
-                            <p className='flex items-center gap-1 text-blue-700'><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /></p>
-                            <h2 className='text-slate-200'>90 min</h2>
-                        </span>
-                    </div>
-                </Link>
-                <Link to='' className='rounded bg-inherit'>
-                    <img src="https://images.pexels.com/photos/7976054/pexels-photo-7976054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""
-                        className='h-48 w-full rounded' />
-                    <div className='w-full text-sm py-2'>
-                        <p className='text-slate-200'>Wonder woman</p>
-                        <span className='flex justify-between items-center'>
-                            <p className='flex items-center gap-1 text-blue-700'><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /></p>
-                            <h2 className='text-slate-200'>90 min</h2>
-                        </span>
-                    </div>
-                </Link>
-                <Link to='' className='rounded bg-inherit'>
-                    <img src="https://images.pexels.com/photos/7976054/pexels-photo-7976054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""
-                        className='h-48 w-full rounded' />
-                    <div className='w-full text-sm py-2'>
-                        <p className='text-slate-200'>Wonder woman</p>
-                        <span className='flex justify-between items-center'>
-                            <p className='flex items-center gap-1 text-blue-700'><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /><BsFillStarFill /></p>
-                            <h2 className='text-slate-200'>90 min</h2>
-                        </span>
-                    </div>
-                </Link>
+            )) : null }
+                
             </div>
         </div>
     )
