0fd4d8be0b4b976626feda5d6d4654605e6226e9
Benson <55843982+Benson-Kim@users.noreply.github.com>
2022-06-22 10:26:16 +0300

    search filter

--- Javascript/React/react-redux-themoviedb/src/index.js
+++ Javascript/React/react-redux-themoviedb/src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import './index.css';
+import App from './App';
+import Movies from './Components/Movies';
+import GenreMovies from './Components/GenreMovies'
+import Movie from './Components/Movie';
+import MovieDetail from './Components/MovieDetail';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App />
+
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route index element={<Movies />} />
+          <Route path='/genreid=:genre' element={<GenreMovies />} />
+          <Route path='/movieid=:movie' element={<MovieDetail />} />
+        </Route>
+      </Routes>
+        
+      </BrowserRouter>
   </React.StrictMode>
 );
 
